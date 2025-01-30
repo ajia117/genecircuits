72d9edc84f148bc20f78f987af1b9cc135c543ed
Tammy Truong <tammytru000@gmail.com>
2025-01-29 18:02:42 -0800

    working electron react app

---
+++ electron-react-flask-app/src/app.tsx
@@ -0,0 +1,5 @@
+import { createRoot } from 'react-dom/client';
+import Body from './body';
+
+const root = createRoot(document.body);
+root.render(<Body />);
---
+++ electron-react-flask-app/src/body.tsx
@@ -0,0 +1,8 @@
+export default function Body() {
+
+    return (
+        <>
+            <h1>Electron-React-Flask Test</h1>
+        </>
+    )
+}
