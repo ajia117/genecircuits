466cbbe47d83e27965991bfafc07a5f93919b08f
Tammy Truong <tammytru000@gmail.com>
2025-01-29 19:09:39 -0800

    working backend requests. fixed cors issue

--- electron-react-flask-app/flask-backend/app.py
+++ electron-react-flask-app/flask-backend/app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from flask_cors import CORS
 
-api = Flask(__name__)
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
-@api.route('/test')
+@app.route('/test')
 def my_profile():
     response_body = {
         "message": "Hello World!"
     }
 
     return response_body
+
+if __name__ == '__main__':
+	app.run(debug=True, port=5000)
