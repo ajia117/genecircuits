d3814ffc4c93b2535658ae7bb8f4f6c2fae991d6
Tammy Truong <tammytru000@gmail.com>
2025-02-04 17:57:10 -0800

    working add node button

--- electron-react-flask-app/src/circuit-builder.tsx
+++ electron-react-flask-app/src/circuit-builder.tsx
@@ -1,5 +1,14 @@
-import React, { useCallback, useState, useRef } from "react";
-import { ReactFlow, Background, Controls, applyEdgeChanges, applyNodeChanges, OnNodesChange, addEdge } from '@xyflow/react';
+import React, { useCallback, useState, useRef, useEffect } from "react";
+import { ReactFlow, 
+    Background, 
+    Controls, 
+    applyEdgeChanges, 
+    applyNodeChanges, 
+    OnNodesChange, 
+    Edge,
+    EdgeChange,
+    Connection,
+    addEdge } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 
 
@@ -8,14 +17,20 @@ const initialNodes = [
         id: '1',
         position: { x: 0, y: 0 }, 
         type: "default",
-        data: { label: 'Hello' }, 
+        data: { label: 'default' }, 
     },
     {
         id: '2',
         position: { x: 100, y: 100 },
-        type: "default",
-        data: { label: 'World' },
+        type: "input",
+        data: { label: 'input' },
     },
+    {
+        id: '3',
+        position: { x: 100, y: 200 },
+        type: "output",
+        data: { label: 'output' },
+    }
 ];
 
 const initialEdges: any[] | (() => any[]) = [];
@@ -33,12 +48,35 @@ export default function CircuitBuilder() {
         (changes: any) => setEdges((eds) => applyEdgeChanges(changes, eds)),
         [],
       );
-      const onConnect = useCallback(
-        (params: any) => setEdges((eds) => addEdge(params, eds)),
-        [],
-      );
+      const onConnect = useCallback((params: any) => {
+        setEdges((eds) => addEdge(params, eds))
+      },[]);
+
+      useEffect(() => {
+        console.log(edges)
+      }, [edges])
+
+      // handler to create a new node. should generalize with params for all the types of custom nodes
+      const createNode = useCallback(() => {
+            setNodes((nodes: any) => {
+                return [
+                    ...nodes,
+                    {
+                        id: Math.random().toString(),
+                        position: {x:200, y:200},
+                        type: "default",
+                        data: { label: "new node" }
+                    },
+                ];
+            })
+        }, []);
+
+        const onEdgeClick = useCallback((event: React.MouseEvent, edge: Edge) => {
+            console.log("Clicked edge ID:", edge.id);
+        }, []);
 
     return(
+        <>
         <div style={{height: 300, width: "100%"}}>
             <ReactFlow
                 nodes={nodes}
@@ -46,10 +84,18 @@ export default function CircuitBuilder() {
                 onNodesChange={onNodesChange}
                 onEdgesChange={onEdgesChange}
                 onConnect={onConnect}
+                onEdgeClick={onEdgeClick}
             >
                 <Background />
                 <Controls />
             </ReactFlow>
         </div>
+
+        {/* TOOLBAR HERE */}
+        <div>
+            <button onClick={createNode}>Add Node</button>
+        </div>
+
+        </>
     );
 }
