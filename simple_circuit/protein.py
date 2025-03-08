53c41131d48dbdaf430036e327269544cda848fd
Sophia Krugler <krugler@usc.edu>
2025-03-08 13:59:32 -0800

    Implement general simuulator with proteins and gates.

--- simple_circuit/protein.py
+++ simple_circuit/protein.py
@@ -1,37 +1,81 @@
+import biocircuits
+
 class Protein:
-    def __init__(self, id, name, initConc, hill, degrad, gates):
+    def __init__(self, id, name, initConc, hill, degrad, gates, extConc = 0.0):
         self.mID = id
         self.mName = name
-        self.mInitialConc = initConc
+        self.mInternalConc = initConc
+        self.mExternalConc = extConc
         self.mHill = hill
         self.mDegradation = degrad
         self.mGates = gates
+    
+    def getHill(self):
+        return self.mHill
 
     def getID(self):
         return self.mID
+
+    def getName(self):
+        return self.mName
+
+    def getExternalConcentration(self):
+        return self.mExternalConc
+        
+    def setExternalConcentration(self, t):
+        # TODO: implement this
+        return
     
-    def getInitialConcentration(self):
-        return self.mInitialConc
+    def getInternalConcentration(self):
+        return self.mInternalConc
+    
+    def setInternalConcentration(self, conc):
+        self.mInternalConc = conc
+
+    def getConcentration(self):
+        return self.mInternalConc + self.mExternalConc
     
     def calcProdRate(self):
         rate = 0.0
+        for gate in self.mGates:
+            rate += gate.regFunc()
+        rate -= self.mDegradation * self.mInternalConc
         return rate
-
-class InputProtein:
-    def __init__(self, id, initConc): # TODO: pass a function for the concentration rather than initConc
-        self.mID = id
-        self.mConc = initConc
-
-    def getID(self):
-        return self.mID
-    
-    def getConcentration(self, t): #TODO: replace with actual concentration
-        return self.mConc
 
 class Gate:
     def __init__(self, type, first, second):
         self.mType = type
         self.mFirstInput = first
         self.mSecondInput = second
+        self.prodRate = 0.0
 
-    
+    def regFunc(self):
+        if self.mType == "act_hill":
+            return biocircuits.act_hill(PROTEIN_ARRAY[self.mFirstInput].getConcentration(), PROTEIN_ARRAY[self.mFirstInput].getHill())
+        elif self.mType == "rep_hill":
+            return biocircuits.rep_hill(PROTEIN_ARRAY[self.mFirstInput].getConcentration(), PROTEIN_ARRAY[self.mFirstInput].getHill())
+        elif self.mType == "aa_and":
+            return biocircuits.aa_and(PROTEIN_ARRAY[self.mFirstInput].getConcentration(), PROTEIN_ARRAY[self.mSecondInput].getConcentration(), PROTEIN_ARRAY[self.mFirstInput].getHill(), PROTEIN_ARRAY[self.mSecondInput].getHill())
+        elif self.mType == "aa_or":
+            return biocircuits.aa_or(PROTEIN_ARRAY[self.mFirstInput].getConcentration(), PROTEIN_ARRAY[self.mSecondInput].getConcentration(), PROTEIN_ARRAY[self.mFirstInput].getHill(), PROTEIN_ARRAY[self.mSecondInput].getHill())
+        elif self.mType == "aa_or_single":
+            return biocircuits.aa_or_single(PROTEIN_ARRAY[self.mFirstInput].getConcentration(), PROTEIN_ARRAY[self.mSecondInput].getConcentration(), PROTEIN_ARRAY[self.mFirstInput].getHill(), PROTEIN_ARRAY[self.mSecondInput].getHill())
+        elif self.mType == "rr_and":
+            return biocircuits.rr_and(PROTEIN_ARRAY[self.mFirstInput].getConcentration(), PROTEIN_ARRAY[self.mSecondInput].getConcentration(), PROTEIN_ARRAY[self.mFirstInput].getHill(), PROTEIN_ARRAY[self.mSecondInput].getHill())
+        elif self.mType == "rr_or":
+            return biocircuits.rr_or(PROTEIN_ARRAY[self.mFirstInput].getConcentration(), PROTEIN_ARRAY[self.mSecondInput].getConcentration(), PROTEIN_ARRAY[self.mFirstInput].getHill(), PROTEIN_ARRAY[self.mSecondInput].getHill())
+        elif self.mType == "rr_and_single":
+            return biocircuits.rr_and_single(PROTEIN_ARRAY[self.mFirstInput].getConcentration(), PROTEIN_ARRAY[self.mSecondInput].getConcentration(), PROTEIN_ARRAY[self.mFirstInput].getHill(), PROTEIN_ARRAY[self.mSecondInput].getHill())
+        elif self.mType == "ar_and":
+            return biocircuits.ar_and(PROTEIN_ARRAY[self.mFirstInput].getConcentration(), PROTEIN_ARRAY[self.mSecondInput].getConcentration(), PROTEIN_ARRAY[self.mFirstInput].getHill(), PROTEIN_ARRAY[self.mSecondInput].getHill())
+        elif self.mType == "ar_or":
+            return biocircuits.ar_or(PROTEIN_ARRAY[self.mFirstInput].getConcentration(), PROTEIN_ARRAY[self.mSecondInput].getConcentration(), PROTEIN_ARRAY[self.mFirstInput].getHill(), PROTEIN_ARRAY[self.mSecondInput].getHill())
+        elif self.mType == "ar_and_single":
+            return biocircuits.ar_and_single(PROTEIN_ARRAY[self.mFirstInput].getConcentration(), PROTEIN_ARRAY[self.mSecondInput].getConcentration(), PROTEIN_ARRAY[self.mFirstInput].getHill(), PROTEIN_ARRAY[self.mSecondInput].getHill())
+        elif self.mType == "ar_or_single":
+            return biocircuits.ar_or_single(PROTEIN_ARRAY[self.mFirstInput].getConcentration(), PROTEIN_ARRAY[self.mSecondInput].getConcentration(), PROTEIN_ARRAY[self.mFirstInput].getHill(), PROTEIN_ARRAY[self.mSecondInput].getHill())
+        else:
+            raise ValueError(f"Unknown regulatory function type: {self.mType}")
+
+
+PROTEIN_ARRAY = [Protein(0, "Protein 0", 0.0, 1, 0.05, [], 1.0), Protein(1, "Protein 1", 0.0, 1, 0.05, [], 2.0), Protein(2, "Protein 2", 0.0, 1, 0.05, [Gate("aa_and", 0, 1)]), Protein(3, "Protein 3", 0.0, 1, 0.05, [], 3), Protein(4, "Protein 4", 0.0, 1, 0.05, [Gate("aa_and", 2, 3)])]
--- simple_circuit/simulate.py
+++ simple_circuit/simulate.py
@@ -1,100 +1,32 @@
-from protein import Protein
-from protein import InputProtein
-from protein import Gate
-import numpy as np
 import scipy.integrate
-import biocircuits
-import bokeh.application
-from   bokeh.io import output_file
-import bokeh.plotting as bp
+from protein import PROTEIN_ARRAY
 
-bokeh.io.output_notebook()
 
 # TODO: leave descriptor
-def simulation_iter(concentrations, t, proteins, inputs):
-    # For each input in inputs, update their current concentration
-    for input in inputs:
-        concentrations[input.getID()] += input.getConcentration(t)
+def simulation_iter(concentrations, t):
+    # print("Concentrations at time: " + str(t))
+    # print(concentrations)
+
+    # Update external concentrations of each protein. Update concentrations of each protein
+    for protein in PROTEIN_ARRAY:
+        protein.setExternalConcentration(t)
+        protein.setInternalConcentration(concentrations[protein.getID()])
 
     # For each protein in proteins, calculate their production rate
-    production_rates = [0.0] * len(proteins)
-    for protein in proteins:
-        production_rates[protein.getID()] = protein.CalcProdRate(concentrations)
+    production_rates = [0.0] * len(PROTEIN_ARRAY)
+    for protein in PROTEIN_ARRAY:
+        production_rates[protein.getID()] = protein.calcProdRate()
 
     # Return the result as a NumPy array
     return production_rates
 
 
-def run_simulation(protein_list, inputs_list, duration):
-    # Number of time points we want for the solutions
-    n = 1000
-
-    # Time points
-    t = np.linspace(0, duration, n)
-
+def run_simulation(t):
     # Initial concentrations each protein
-    initial_concentrations = [0.0] * len(protein_list)
-    for protein in protein_list:
-        initial_concentrations[protein.getID()] = protein.getInitialConcentration()
-
-    args = (protein_list, inputs_list) # each input should also have a function corresponding to its concentration
+    initial_concentrations = [0.0] * len(PROTEIN_ARRAY)
+    for protein in PROTEIN_ARRAY:
+        initial_concentrations[protein.getID()] = protein.getInternalConcentration()
 
     # Integrate!
-    final_concentrations =  scipy.integrate.odeint(simulation_iter, initial_concentrations, t, args=args)
+    final_concentrations =  scipy.integrate.odeint(simulation_iter, initial_concentrations, t)
     return final_concentrations
-
-
-
-"""
-TODO:
-Create a list of proteins and inputs
-Call run_simulation
-Show results
-"""
-
-# Create a list of proteins
-proteins = [Protein(0, 0.0, 1, 0.05, [Gate('activation', 1, 0)]), Protein(1, 0.0, 4, 0.05, [Gate('activation', 0, 1)])]
-
-# Create a list of inputs
-inputs = [InputProtein(0), InputProtein(2)]
-
-# Run the simulation
-duration = 20
-final_concentrations = run_simulation(proteins, inputs, duration)
-
-# Set up color palette
-colors = bokeh.palettes.d3['Category10'][10]
-
-# Pluck out x, y and z
-transposed_conc = final_concentrations.transpose()
-
-# Set up plot
-plot = bp.figure(width=500,
-                          height=300,
-                          x_axis_label='dimensionless time',
-                          y_axis_label='dimensionless concentrations')
-
-# Place the legend
-plot.legend.location = 'bottom_right'
-
-for i in range(len(proteins)):
-    protein = proteins[i]
-    conc = transposed_conc[i]
-    plot.line(duration, conc, line_width=2, color=colors[i], legend_label=protein.getID())
-
-
-# Plot the input proteins
-# TODO: replace mConc with the concentration function
-for i in range(len(inputs)):
-    input = inputs[i]
-    plot.line(duration, input.mConc, line_width=2, color=colors[i + len(proteins)], legend_label=input.getID())
-
-p_x = bp.figure(width=500,
-                          height=300,
-                          x_axis_label='dimensionless time',
-                          y_axis_label='dimensionless x')
-
-#Show plot
-output_file("plots/simulation_results.html")
-layout = bokeh.layouts.column(plot, p_x)
-bokeh.io.show(layout)
