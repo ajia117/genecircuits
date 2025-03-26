4a3ad733659e17440e8d963a6f6db1380731a282
Tammy Truong <tammytru000@gmail.com>
2025-03-25 17:50:20 -0700

    fixed svg asset properties stroke-line > strokeLine

--- src/assets/Trash.tsx
+++ src/assets/Trash.tsx
@@ -1,11 +1,11 @@
 const Trash = () => {
     return (
 <svg width="24" height="24" viewBox="0 0 21 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16.896 4.42408H3.13145V19.2284C3.13145 20.4806 4.16351 21.5 5.43115 21.5H14.585C15.8527 21.5 16.8848 20.4806 16.8848 19.2284V4.42408H16.896Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M1 4.42408H19.42" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M13.6652 1H6.75485V4.41297H13.6652V1Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M7.89911 8.76785V16.9568" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M12.5097 8.76785V16.9568" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M16.896 4.42408H3.13145V19.2284C3.13145 20.4806 4.16351 21.5 5.43115 21.5H14.585C15.8527 21.5 16.8848 20.4806 16.8848 19.2284V4.42408H16.896Z" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M1 4.42408H19.42" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M13.6652 1H6.75485V4.41297H13.6652V1Z" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M7.89911 8.76785V16.9568" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M12.5097 8.76785V16.9568" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
 
 
