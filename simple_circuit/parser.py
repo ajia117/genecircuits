231aa564d59da3074c2e4b3b2c756931009215b0
Hallie Faust <hfaust@usc.edu>
2025-03-26 11:03:47 -0700

    parser fixes

--- simple_circuit/parser.py
+++ simple_circuit/parser.py
@@ -13,11 +13,12 @@ def parse_circuit(json_data):
     inputs = defaultdict(list)
     for edge in edges:
         inputs[edge['target']].append(edge['source'])
-    print(inputs)
 
     protein_array = []
     node_to_protein_idx = {}
     current_protein_id = 0
+
+    smush_gates = {}
 
     for node_id, node in nodes.items():
         node_type = node['type']
@@ -28,28 +29,28 @@ def parse_circuit(json_data):
 
         gates = []
 
-        if node_type in ['default']:
+        if node_type in ['default', 'output']:
             # Determine the input sources to this protein
             input_sources = inputs.get(node_id, [])
             for src in input_sources:
-                gates.append(Gate("act_hill", int(src), int(src)))
+                if src in smush_gates:
+                    gates.append(smush_gates[src])
+                else:
+                    gates.append(Gate("act_hill", int(src), int(src)))
             protein = Protein(current_protein_id, label, init_conc, hill, degrad, gates)
             node_to_protein_idx[node_id] = current_protein_id
             protein_array.append(protein)
             current_protein_id += 1
 
         elif node_type in ['and', 'or']:
-            print("true")
-            print(node_id)
             gate_type = 'aa_and' if node_type == 'and' else 'aa_or'
             input_sources = inputs.get(node_id, [])
-            print(input_sources)
             if len(input_sources) >= 2:
                 first, second = int(input_sources[0]), int(input_sources[1])
-                gates.append(Gate(gate_type, first, second))
-            protein = Protein(current_protein_id, label, init_conc, hill, degrad, gates)
-            node_to_protein_idx[node_id] = current_protein_id
-            protein_array.append(protein)
+                smush_gates[node_id] = Gate(gate_type, first, second)
+            # protein = Protein(current_protein_id, label, init_conc, hill, degrad, gates)
+            # node_to_protein_idx[node_id] = current_protein_id
+            # protein_array.append(protein)
             current_protein_id += 1
 
         elif node_type == 'input':
@@ -58,9 +59,6 @@ def parse_circuit(json_data):
             node_to_protein_idx[node_id] = current_protein_id
             protein_array.append(protein)
             current_protein_id += 1
-
-        elif node_type == 'output':
-            continue  # Outputs are not proteins
 
     return protein_array
 
@@ -71,5 +69,5 @@ if __name__ == "__main__":
 
     proteins = parse_circuit(data)
     for p in proteins:
-        print(f"Node ID: {p.mID}, Name: {p.mName}, Gates: {[ (g.mType, g.mFirstInput, g.mSecondInput) for g in p.mGates ]}")
+        print(f"Protein ID: {p.mID}, Name: {p.mName}, {(p.mInternalConc, p.mHill, p.mDegradation)}, Gates: {[ (g.mType, g.mFirstInput, g.mSecondInput) for g in p.mGates ]}")
 
