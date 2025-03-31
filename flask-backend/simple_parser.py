dd30da5e9f00a43233ec06ebc26f7f8d520feb92
Tammy Truong <tammytru000@gmail.com>
2025-03-30 18:35:24 -0700

    added logger to backend

--- flask-backend/simple_parser.py
+++ flask-backend/simple_parser.py
@@ -4,6 +4,11 @@ from collections import defaultdict
 
 # Parser function
 def parse_circuit(json_data):
+    # Write protein_array to a log file
+    with open("protein_log.txt", "w") as log_file:
+        log_file.write("Received json" + "\n")
+        log_file.write(json.dumps(json_data, indent=4) + "\n")
+    
     id_map = {}
     currNodeID = 0
     currGateID = 0
@@ -42,6 +47,8 @@ def parse_circuit(json_data):
             if len(input_sources) >= 2:
                 first, second = int(input_sources[0]), int(input_sources[1])
                 smush_gates[node_id] = Gate(gate_type, first, second)
+            else:
+                raise ValueError(f"Gate '{node_id}' ({node['type']}) must have at least two inputs, but got {len(input_sources)}.")
 
     for node_id, node in nodes.items():
         node_type = node['type']
@@ -59,25 +66,44 @@ def parse_circuit(json_data):
                 if src in smush_gates:
                     gates.append(smush_gates[src])
                 else:
+                    if len(input_sources) != 1:
+                        raise ValueError(f"Gate '{node_id}' ({node['type']}) must have exactly one input, but got {len(input_sources)}.")
                     gates.append(Gate("act_hill", int(src), int(src)))
+            # Check if the protein already exists
+            found_protein = next((protein for protein in protein_array if protein.mName == label), None)
+            if found_protein:
+                found_protein.mGates.extend(gates)
+                continue
+            
+            # Otherwise, create a new protein
             protein = Protein(current_protein_id, label, init_conc, hill, degrad, gates)
             protein_array.append(protein)
             current_protein_id += 1
 
         elif node_type == 'input':
             # External protein
+            found_protein = next((protein for protein in protein_array if protein.mName == label), None)
+            if found_protein:
+                # TODO: read in the user defined function for input concentration
+                continue
+            
             protein = Protein(current_protein_id, label, init_conc, hill, degrad, [])
             protein_array.append(protein)
             current_protein_id += 1
 
+    # Write protein_array to a log file
+    with open("protein_log.txt", "a") as log_file:
+        log_file.write("Parsed protein list" + "\n")
+        for protein in protein_array:
+            log_file.write(f"Protein ID: {protein.mID}, Name: {protein.mName}, Hill: {protein.mHill}, Degradation: {protein.mDegradation}, Gates: {[ (g.mType, g.mFirstInput, g.mSecondInput) for g in protein.mGates ]}\n")
+    
     return protein_array
 
 #Example usage
 if __name__ == "__main__":
-    with open("animals.json") as f:
+    with open("colors.json") as f:
         data = json.load(f)
 
     proteins = parse_circuit(data)
     for p in proteins:
         print(f"Protein ID: {p.mID}, Name: {p.mName}, {(p.mInternalConc, p.mHill, p.mDegradation)}, Gates: {[ (g.mType, g.mFirstInput, g.mSecondInput) for g in p.mGates ]}")
-
