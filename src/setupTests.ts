c03987b7bdd6d768d92b553acc872087350788f7
Shriya Upadhyay <shriyaup@usc.edu>
2025-11-22 01:37:31 -0500

    undo changes to setuptests.js

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,25 +1,3 @@
 // Jest setup file
 import '@testing-library/jest-dom';
 
-/**
- * JSDOM (used by Jest) does not implement ResizeObserver yet.
- * Provide a minimal mock so components relying on Radix hooks
- * can mount without throwing.
- */
-const mockResizeObserver = class {
-    observe() {
-        // noop
-    }
-    unobserve() {
-        // noop
-    }
-    disconnect() {
-        // noop
-    }
-};
-
-if (typeof window !== 'undefined') {
-    (window as any).ResizeObserver = mockResizeObserver;
-}
-
-(global as any).ResizeObserver = mockResizeObserver;
